e8fd037fb8202755841df86f23c97005ed4eb8c0
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2021-12-13 22:54:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+/* TOGGLE */
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+scrollReveal.reveal(
+    `#home .fundo, #about .fundo-2
+    `, {
+        interval: 100
+    }
+)
