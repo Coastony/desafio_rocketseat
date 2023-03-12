3310aff03e200c3d2a9d236a2f8e99daac65c07a
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2023-03-12 09:46:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-/* TOGGLE */
+const btnMobile = document.getElementById("btn-mobile");
 
-const btnMobile = document.getElementById('btn-mobile');
-
+// Toggle nav
 function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
+  if (event.type === "touchstart") event.preventDefault();
+
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+
+  event.currentTarget.setAttribute("aria-expanded", active);
+
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
-scrollReveal.reveal(
-    `#home .fundo, #about .fundo-2
-    `, {
-        interval: 100
-    }
-)
+// Scroll Reveal
+
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.square, .earth', { delay: 300, origin: 'right'});
+ScrollReveal().reveal('.square_kid, .kid', { delay: 300, origin: 'left'});
+ScrollReveal().reveal('.img_service', { delay: 300, origin: 'top'});
+ScrollReveal().reveal('.card', { delay: 300, origin: 'top'});
+ScrollReveal().reveal('.lets-right, .lets-left', { delay: 300, origin: 'left'});
